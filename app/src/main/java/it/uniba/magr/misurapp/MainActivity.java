fbf636ce7c43dd8b65fd862cb9a2e3217000be91
gamerover98 <gamerover98@hotmail.it>
2020-10-22 22:37:40 +0200

    Added ActionBar (ToolBar) and Navigation to the MainActivity with examples

--- app/src/main/java/it/uniba/magr/misurapp/MainActivity.java
+++ app/src/main/java/it/uniba/magr/misurapp/MainActivity.java
@@ -1,16 +1,104 @@
 package it.uniba.magr.misurapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.internal.NavigationMenuItemView;
+import com.google.android.material.navigation.NavigationView;
+
+@SuppressWarnings("FieldCanBeLocal") // this suppression will be when lombok plugin will be added.
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
+
+    private NavController navController;
+
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigation();
+
+    }
+
+    /**
+     * Setup the ActionBar with the hamburger button and
+     * the navigation menu from left side.
+     */
+    private void setupNavigation() {
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //ActionBar is initialized by toolbar.
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        drawerLayout   = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    /*
+     * Hamburger button click event.
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, drawerLayout);
+    }
+
+    /*
+     * Back arrow button click event.
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed(); // unused for now.
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        item.setChecked(true);
+        drawerLayout.closeDrawers();
+        int id = item.getItemId();
+
+        NavigationMenuItemView mainMenuItem = findViewById(R.id.drawer_menu_main);
+        NavigationMenuItemView secondMenuItem = findViewById(R.id.drawer_menu_second);
+
+        if (id == mainMenuItem.getId()) {
+            navController.navigate(R.id.nav_main_fragment);
+        } else if (id == secondMenuItem.getId()) {
+            navController.navigate(R.id.nav_second_fragment);
+        }
+
+        return false;
 
     }
 
---
+++ app/src/main/java/it/uniba/magr/misurapp/MainFragment.java
@@ -0,0 +1,35 @@
+package it.uniba.magr.misurapp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup parent,
+                             @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.main_layout_fragment,
+                parent, false);
+
+    }
+
+}
