a890c7902d22b8da89462c10bd80964f686a3deb
Gip94 <giu.viola94@gmail.com>
2020-10-27 18:18:00 +0100

    Reduced introduction complexity by internal fragment class paging

--- app/src/main/java/it/uniba/magr/misurapp/introduction/IntroPagerAdapter.java
+++ app/src/main/java/it/uniba/magr/misurapp/introduction/IntroPagerAdapter.java
@@ -1,39 +1,62 @@
 package it.uniba.magr.misurapp.introduction;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
-import it.uniba.magr.misurapp.introduction.page.FirstPage;
-import it.uniba.magr.misurapp.introduction.page.SecondPage;
-import it.uniba.magr.misurapp.introduction.page.ThirdPage;
+import it.uniba.magr.misurapp.R;
 
 public class IntroPagerAdapter extends FragmentStatePagerAdapter {
 
     private static final int PAGES = 3;
 
-    private final Fragment[] pages = new Fragment[PAGES];
+    private final Fragment[] pagesArray = new Fragment[PAGES];
 
+    @SuppressWarnings("squid:S1874")
     public IntroPagerAdapter(@NonNull FragmentManager fragmentManager) {
 
         super(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
 
-        pages[0] = new FirstPage();
-        pages[1] = new SecondPage();
-        pages[2] = new ThirdPage();
+        pagesArray[0] = new PageFragment(R.layout.intro_first_page);
+        pagesArray[1] = new PageFragment(R.layout.intro_second_page);
+        pagesArray[2] = new PageFragment(R.layout.intro_third_page);
 
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return pages[position];
+        return pagesArray[position];
     }
 
     @Override
     public int getCount() {
-        return pages.length;
+        return pagesArray.length;
+    }
+
+    public static class PageFragment extends Fragment {
+
+        private final int layoutID;
+
+        public PageFragment(int layoutID) {
+            this.layoutID = layoutID;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 @Nullable ViewGroup parent,
+                                 @Nullable Bundle bundle) {
+            return inflater.inflate(layoutID, parent, false);
+        }
+
     }
 
 }
