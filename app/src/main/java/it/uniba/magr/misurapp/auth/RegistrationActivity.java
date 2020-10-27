37c87d8cc997b409278b41cbe3953dbdbb0599fa
Giuseppe Viola <g.viola8@studenti.uniba.it>
2020-10-27 18:00:08 +0100

    Added Login and Registration Activities and layouts, fixed some wrong names and added/fixed strings

---
+++ app/src/main/java/it/uniba/magr/misurapp/auth/RegistrationActivity.java
@@ -0,0 +1,21 @@
+package it.uniba.magr.misurapp.auth;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import it.uniba.magr.misurapp.R;
+
+@SuppressWarnings("squid:S110")
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+    }
+
+}
