50ba1cacfa94e4778123c7bfa1b1129625d8c048
RaffSStein <raffaele.princee@gmail.com>
2021-03-19 17:23:27 +0100

    Added the barometer and pedometer cards in tools list Added icons and strings of these tools

---
+++ app/src/main/java/it/uniba/magr/misurapp/navigation/tools/list/card/BarometerToolCard.java
@@ -0,0 +1,46 @@
+package it.uniba.magr.misurapp.navigation.tools.list.card;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import androidx.navigation.NavController;
+
+import org.jetbrains.annotations.NotNull;
+
+import it.uniba.magr.misurapp.HomeActivity;
+import it.uniba.magr.misurapp.R;
+
+public class BarometerToolCard implements ToolCard {
+
+    @Override
+    public int getImageID() {
+        return R.drawable.icon_barometer;
+    }
+
+    @Override
+    public int getTitleID() {
+        return  R.string.text_barometer;
+    }
+
+    @Override
+    public void onClick(@NotNull Context context) {
+
+        HomeActivity activity = (HomeActivity) context;
+        NavController navController = activity.getNavController();
+
+        navController.navigate(R.id.action_nav_add_measure_fragment_to_barometer);
+
+    }
+
+    @Override
+    public boolean isSupported(@NotNull Context context) {
+
+        SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor barometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+
+        return barometerSensor != null;
+
+    }
+
+}
---
+++ app/src/main/java/it/uniba/magr/misurapp/navigation/tools/list/card/PedometerToolCard.java
@@ -0,0 +1,46 @@
+package it.uniba.magr.misurapp.navigation.tools.list.card;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import androidx.navigation.NavController;
+
+import org.jetbrains.annotations.NotNull;
+
+import it.uniba.magr.misurapp.HomeActivity;
+import it.uniba.magr.misurapp.R;
+
+public class PedometerToolCard implements ToolCard {
+
+    @Override
+    public int getImageID() {
+        return R.drawable.icon_pedometer;
+    }
+
+    @Override
+    public int getTitleID() {
+        return  R.string.text_pedometer;
+    }
+
+    @Override
+    public void onClick(@NotNull Context context) {
+
+        HomeActivity activity = (HomeActivity) context;
+        NavController navController = activity.getNavController();
+
+        navController.navigate(R.id.action_nav_add_measure_fragment_to_pedometer);
+
+    }
+
+    @Override
+    public boolean isSupported(@NotNull Context context) {
+
+        SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor pedometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        return pedometerSensor != null;
+
+    }
+
+}
